9e002fc0e8e226f3de7723eb01661d6952e0e2d2
AkhilPillai0 <akhilpillai19996@gmail.com>
2021-07-14 13:27:55 +0530

    Final backend

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -25,6 +25,31 @@ const song = mongoose.model("Song", songSchema);
 
 app.get('/songs', (req,res) => {
     song.find().then(songs => res.json(songs));
+})
+
+app.post('/newsong', (req,res) => {
+    const title = req.body.title;
+    const genre = req.body.genre;
+    const year = req.body.year;
+
+    const newSong = new song({
+        title,
+        genre,
+        year
+    });
+
+    newSong.save();
+})
+
+app.delete('/delete/:id', (req,res) => {
+    const id = req.params.id;
+    song.findByIdAndDelete({_id: id}, (err) => {
+        if(!err){
+            console.log("movie deleted");
+        } else {
+            console.log(err);
+        }
+    })
 })
 
 app.listen(port,function() {
